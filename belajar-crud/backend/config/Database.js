9d5749a3c78ad23225c36d75087f3f9ac2cd3d14
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2022-07-01 10:37:35 +0700

    first learning

--- belajar-crud/backend/config/Database.js
+++
@@ -1,8 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize('crud_express','Hello666Sec', 'ProgrammerRaincity', {
-    host: "localhost",
-    dialect: "mysql",
-});
-
-export default db;
