c1ef5952c6eec14086339fbf621c25ef33f085da
Juang999 <juangraharjo03@gmail.com>
2022-12-08 21:55:33 +0700

    update learning

--- learnExpressjs/app/http/Middleware/TestMiddleware.js
+++ learnExpressjs/app/http/Middleware/TestMiddleware.js
@@ -6,6 +6,13 @@ const middleware = {
     testMIddleware2: (req, res, next) => {
         console.log('hallo dunia')
         next()
+    },
+    errMiddleware: (req, res, err, next) => {
+        console.log(err.stack)
+        res.status(500).json({
+            status: "gagal",
+            message: "testing error middleware"
+        })
     }
 }
 
--- learnExpressjs/routes/api.js
+++ learnExpressjs/routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { TestController } = require('../app/http/controllers/Controller');
 const expressMiddleware = express();
 const router = express.Router();
 const controllers = require("../app/http/controllers/Controller")
@@ -6,16 +7,28 @@ const middleware = require("../app/http/Middleware/TestMiddleware")
 
 
 
-expressMiddleware.use('/api/user', middleware.testMiddleware, middleware.testMIddleware2)
+expressMiddleware.use('/api/user/:id', middleware.errMiddleware)
 // withoutId
 router.route('/user')
-    .get(controllers.TestController.index)
-    .post(controllers.TestController.store)
+.get(controllers.TestController.index)
+.post(controllers.TestController.store)
 
 // withId
+// const useMiddleware = [middleware.testMiddleware, middleware.testMIddleware2]
+// function testMiddleware (req, res, next) {
+//     console.log('hello world')
+//     next()
+// }
+// function testMIddleware2 (req, res, next) {
+//     console.log('hallo dunia')
+//     next()
+// }
+
+const impMiddleware = [middleware.errMiddleware]
 router.route('/user/:id')
-    .get(controllers.TestController.show)
+    .get(impMiddleware, controllers.TestController.show)
     .put(controllers.TestController.update)
     .delete(controllers.TestController.destroy)
+// router.get('/user/:id', impMiddleware, TestController.show)
 
 module.exports = {router, expressMiddleware};
