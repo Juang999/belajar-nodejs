40d2945d3de3dce98261d6be5d6a634c7aa21e71
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-09-18 20:50:04 +0700

    update pembelajaran

--- unit-test/learning/src/sum.js
+++ unit-test/learning/src/sum.js
@@ -1,3 +1,12 @@
 export const sum = (first, second) => {
     return first + second
 }
+
+export const sumAll = (numbers, callback) => {
+    let total = 0
+    for (number of numbers) {
+        total += number
+    }
+
+    callback(total)
+}
---
+++ unit-test/learning/test/mock_function.js
@@ -0,0 +1,5 @@
+
+
+test("test calculate", () => {
+	const callback = jest.fn()
+})
