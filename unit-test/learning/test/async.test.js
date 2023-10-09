0cbd9e221a01199290efea4219e21b3a8d0e6079
Bangkit Juang Raharjo <bangkitjuangraharjo@gmail.com>
2023-10-10 05:40:00 +0700

    update learning

--- unit-test/learning/test/async.test.js
+++
@@ -1,6 +0,0 @@
-import {asyncSayHello} from "../src/async"
-
-test("test async", async () => {
-    await expect(asyncSayHello("Bangkit Juang Raharjo")).resolves.toBe("hello Bangkit Juang Raharjo")
-    await expect(asyncSayHello("Budiman Yusuf")).rejects.toBe("isn't your name!")
-})
--- unit-test/learning/test/string.test.js
+++
@@ -1,7 +0,0 @@
-test("test string for unit test", () => {
-    let name = "Bangkit Juang Raharjo"
-
-    expect(name).toBe("Bangkit Juang Raharjo")
-    expect(name).toEqual("Bangkit Juang Raharjo")
-    expect(name).toMatch(/Juang/)
-})
